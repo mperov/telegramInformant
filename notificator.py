c54151bc02682c01f9b280fa4d87686fe3ff7bbe
Maksim Perov <coder@frtk.ru>
2022-12-12 22:49:18 +0300

    checking hosts are got from json file

--- notificator.py
+++ notificator.py
@@ -7,6 +7,8 @@
 
 import telebot
 import requests
+import socket
+import json
 from time import sleep
 
 TOKEN=''
@@ -19,9 +21,49 @@ bot = telebot.TeleBot(TOKEN)
 def get(message):
     print(message.text)
 
-while True:
+def opened(ip, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(1)
     try:
-        response = requests.get("https://pass.mipt.ru:2443", timeout=30)
+        s.connect((ip, int(port)))
+        s.shutdown(socket.SHUT_RDWR)
+        return True
     except:
-        bot.send_message(GROUP_ID, "ALERT: pass.mipt.ru:2443 isn't available!")
-    sleep(TIMEOUT)
+        return False
+    finally:
+        s.close()
+
+def check_host(ip, port):
+    ipup = False
+    for i in range(5):
+        if opened(ip, port):
+            ipup = True
+            break
+        else:
+            sleep(3)
+    return ipup
+
+def alert(hostname, _socket):
+    bot.send_message(GROUP_ID, "ALERT: " + hostname + "( " + _socket + " ) isn't available!")
+
+if __name__ == "__main__":
+    resources = {}
+    try:
+        with open("resources.json", "r") as file:
+            resources = json.load(file)
+    except:
+        exit(-1)
+    if resources != {}:
+        while True:
+            for hostname in resources:
+                res = resources[hostname]
+                _socket = res['host'] + ":" + res['port']
+                if res['type'] in ['https', 'http']:
+                    try:
+                        response = requests.get(res['type'] + "://" + _socket, timeout=30)
+                    except:
+                        alert(hostname, res['type'] + "://" + _socket)
+                else:
+                    if not check_host(res['host'], res['port']):
+                        alert(hostname, _socket)
+            sleep(TIMEOUT)
