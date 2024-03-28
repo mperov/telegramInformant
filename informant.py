836d106d2b1612173ef2ec62b15fdbca9eaec006
Maksim Perov <coder@frtk.ru>
2024-03-29 00:19:58 +0300

    bot can control hosts which should be available

--- informant.py
+++ informant.py
@@ -45,36 +45,46 @@ def check_host(ip, port):
             sleep(CONNECT_TIMEOUT)
     return ipup
 
-def alert(hostname, _socket, available = False):
+def alert(hostname, _socket, available = False, hidden = False):
     if not available:
         postfix = " ) isn't available!"
+        if hidden:
+            postfix += "\nIt is okay, because resource should be unavailable."
     else:
         postfix = " ) is available!"
+        if hidden:
+            postfix += "\nAHTUNG, because resource should be unavailable!!!"
     bot.send_message(GROUP_ID, "ALERT: " + hostname + "( " + _socket + postfix)
 
 def routine(resources):
     for hostname in resources:
         res = resources[hostname]
+        hidden = False
         if not 'available' in res:
-            res.update({'available' : True})
+            value = True
+            if 'hidden' in res:
+                if res['hidden'] == "yes":
+                    value = False
+                    hidden = True
+            res.update({'available' : value})
         _socket = res['host'] + ":" + res['port']
         if res['type'] in ['https', 'http']:
             try:
                 response = requests.get(res['type'] + "://" + _socket, timeout=30)
                 if not res['available']:
-                    alert(hostname, _socket, True)
+                    alert(hostname, _socket, True, hidden)
                     res['available'] = True
             except:
                 if res['available']:
-                    alert(hostname, res['type'] + "://" + _socket)
+                    alert(hostname, res['type'] + "://" + _socket, False, hidden)
                     res['available'] = False
         else:
             if not check_host(res['host'], res['port']):
                 if res['available']:
-                    alert(hostname, _socket)
+                    alert(hostname, _socket, False, hidden)
                     res['available'] = False
             elif not res['available']:
-                alert(hostname, _socket, True)
+                alert(hostname, _socket, True, hidden)
                 res['available'] = True
 
 if __name__ == "__main__":
@@ -86,7 +96,7 @@ if __name__ == "__main__":
         print("Input data error!")
         exit(-1)
     if resources != {}:
-        bot.send_message(GROUP_ID, "Hello! I'm Telegram bot! It's my first running! I'll notify you if your hosts will be unavailable!")
+        bot.send_message(GROUP_ID, "Hello! I'm Telegram bot! It's my first running! I'll notify you about hosts availability!")
         while True:
             routine(resources)
             sleep(GLOBAL_TIMEOUT)
